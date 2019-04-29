8ae92237fa5467011e0f5545f36b165817ed8f22
Matheus Zenker <matheus.g.zenker@gmail.com>
2019-04-29 18:54:00 -0300

    First

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'nodegobarber.mysql.dbaas.com.br',
+  username: 'nodegobarber',
+  password: 'l3tm3h3lpu',
+  database: 'nodegobarber',
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
